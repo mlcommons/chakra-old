e8be06ea6f4bc26931c82d8e67cd91b84f72d9c9
Changhai <changhai0109@hotmail.com>
2023-10-03 12:20:14 -0400

    changed window size of ET feeder, temporary solution for not loading whole graph.

--- et_feeder/et_feeder.cpp
+++ et_feeder/et_feeder.cpp
@@ -5,7 +5,9 @@ using namespace std;
 using namespace Chakra;
 
 ETFeeder::ETFeeder(string filename)
-    : trace_(filename), window_size_(4096), et_complete_(false) {
+    : trace_(filename), window_size_(__UINT32_MAX__ >> 2), et_complete_(false) {
+  // TODO: BUG of window_size_, when small, the graph is not complete even after
+  // all node has dep resolved. Should be a problem in depResolve
   readNextWindow();
 }
 
